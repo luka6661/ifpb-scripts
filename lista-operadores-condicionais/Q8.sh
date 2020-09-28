10b744120049be9c4ed96d1c141cbb319262eb55
Luka <luka.mendonca@academico.ifpb.edu.br>
2020-09-28 13:43:49 -0300

    corrigindo Q8.sh

--- lista-operadores-condicionais/Q8.sh
+++ lista-operadores-condicionais/Q8.sh
@@ -3,7 +3,7 @@
 # [PESQUISA] Quando usamos o pipe (|) para conectar dois ou mais comandos, a variável $? toma o valor de sucesso ou falha do último comando da sequência. Como verificar o sucesso/falha dos outros comandos na sequência do pipe.
 
 
-cat a.txt | tr 'f' 'x' \
-	| sort && echo $? || echo $? \
-	| grep -v "x" && echo $? || echo $?
+cat a.txt | tr '.' ',' | \
+	 sort && echo $? || echo $? | \
+	 grep -v "x" && echo $? || echo $?
 
