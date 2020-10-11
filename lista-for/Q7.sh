ce7c52ef08d336c22f96a78ba6f1e50d73786078
Luka <luka.mendonca@academico.ifpb.edu.br>
2020-10-11 20:16:43 -0300

    Refazendo Q7

--- lista-for/Q7.sh
+++ lista-for/Q7.sh
@@ -5,12 +5,28 @@
 # arquivos com o mesmo conteúdo no diretório atual.
 # Caso existam, imprima o nome dos arquivos duplicados.
 
-md5sum * | cut -d' ' -f1 > md5files.txt
+#md5sum * | cut -d' ' -f1 > md5files.txt
+#
+#for line in $(cat md5files.txt); do
+#	for another in $(cat md5files.txt); do
+#		(( $line == $another ))
+#	done
+#done
 
-for line in $(cat md5files.txt); do
-	for another in $(cat md5files.txt); do
-		(( $line == $another ))
-	done
+#rm md5files.txt
+
+declare -A hashs
+
+for f in *; do
+  hash="$(md5sum $f | cut -d' ' -f1)"
+
+  # Verifica se hash já existe
+  # caso exista é duplicado
+  # senao é adicionado ao array
+  if [[ -z "${hashs[$hash]}" ]]; then
+    hashs[$hash]="$f"
+  else
+    echo "arquivos duplicados: '${hashs[$hash]}' e '$f'"
+  fi
 done
 
-rm md5files.txt
